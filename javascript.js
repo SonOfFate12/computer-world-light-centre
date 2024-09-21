73eba6ed00fa6791524d59b382b0fac07b15eb8a
SonOfFate12 <140921850+SonOfFate12@users.noreply.github.com>
2024-09-21 18:40:46 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
 //time
 setInterval(function() {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = minutes.toString().padStart(2, '0');
-  seconds = seconds.toString().padStart(2, '0');
-  var time = `${hours}:${minutes}:${seconds} ${ampm}`;
-  document.getElementById("time").innerHTML = time;
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes.toString().padStart(2, '0');
+  seconds = seconds.toString().padStart(2, '0');
+  var time = `${hours}:${minutes}:${seconds} ${ampm}`;
+  document.getElementById("time").innerHTML = time;
 }, 1000);
 
 //shopping cart
@@ -18,23 +18,40 @@ let cartCount = 0;
 const maxCartCount = 10;
 
 function updateCartCount() {
-  document.getElementById("cart-count").innerHTML = cartCount;
+  document.getElementById("cart-count").innerHTML = cartCount;
 }
 
 function addItemToCart() {
-  if (cartCount < maxCartCount) {
-    cartCount++;
-    updateCartCount();
-  } else {
-    alert("Cart is full!");
-  }
+  if (cartCount < maxCartCount) {
+    cartCount++;
+    updateCartCount();
+  } else {
+    alert("Cart is full!");
+  }
 }
 
 function removeItemFromCart() {
-  if (cartCount > 0) {
-    cartCount--;
-    updateCartCount();
-  }
+  if (cartCount > 0) {
+    cartCount--;
+    updateCartCount();
+  }
 }
 
+//form
+    const form = document.getElementById('contact-form');
 
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const message = document.getElementById('message').value;
+
+      emailjs.send('service_x0wcr44', 'template_lkzosrj', {
+        message: message,
+      })
+      .then((response) => {
+        console.log('Email sent successfully', response);
+      })
+      .catch((error) => {
+        console.error('Error sending email', error);
+      });
+    });
